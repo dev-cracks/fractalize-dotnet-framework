6452d7026263630c1a9d8014c6d34b5d3bd81ccd
Daniel F Yepes <dyepes@hiberus.com>
2025-05-24 02:42:14 +0200

    Base type for entities and events

---
+++ src/DevCracks.Fractalize.Domain/Base/EventBase.cs
@@ -0,0 +1,10 @@
+﻿namespace DevCracks.Fractalize.Domain.Base;
+
+public abstract class EventBase<EnumType> where EnumType : struct, Enum
+{
+    public DateTimeOffset CreatedAt { get; set; } = DateTimeOffset.UtcNow;
+    public EnumType EventType { get; set; }
+    public string EventId { get; set; } = Guid.NewGuid().ToString();
+    public string? CorrelationId { get; set; }
+    public string? CausationId { get; set; }
+}
